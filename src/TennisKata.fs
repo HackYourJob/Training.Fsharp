c72903b1dfff9322fff363a75668ac9c94d442b2
lsaudon <lsaudon@gmail.com>
2020-12-14 16:20:43 +0100

    WIP

--- src/TennisKata.fs
+++ src/TennisKata.fs
@@ -1,9 +1,24 @@
 module HYJ.Formation.TennisKata
 
 module Implementation =
-    let run v =
-        v.ToString()
+    type Player = Player1 | Player2
+    type Point =
+        | Love
+        | Fifteen
+        | Thirty
+    type Score =
+        | Points of Point * Point
+        | Fourty of Player * Point
+        | Deuce
+        | Advantage of Player
+        | Game of Player
+    type OldScore = Score
+    type Play = OldScore -> Player -> Score
 
+    let play oldScore player =
+        match player with
+        | Player1 -> (Fifteen, Love)
+        | Player2 -> (Fifteen, Fifteen)
 
 module Tests =
     open Xunit
@@ -12,5 +27,9 @@ module Tests =
     open Implementation
 
     [<Fact>]
-    let ``should return number`` () =
-        test <@ run 2 = "2" @>
+    let ``should return Fifteen Love when Player1 win``() =
+        test <@ play (Love, Love) Player1 = (Fifteen, Love) @>
+
+    [<Fact>]
+    let ``should return Fifteen Fifteen when Player2 win and previous score Fifteen Love``() =
+        test <@ play (Fifteen, Love) Player2 = (Fifteen, Fifteen) @>
